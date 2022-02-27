afeee7ac646247d28487c3b4aa8e2836a73675e1
Tomasz Klara <tomaszklara@gmail.com>
2022-02-27 11:46:51 +0000

    updated sql

--- migrations/000001_create_records_table.down.sql
+++ migrations/000001_create_records_table.down.sql
@@ -1,15 +1,5 @@
-ALTER TABLE record_artists DROP CONSTRAINT fk_artists;
-ALTER TABLE record_artists DROP CONSTRAINT fk_records;
-
-ALTER TABLE record_genres DROP CONSTRAINT fk_records;
-ALTER TABLE record_genres DROP CONSTRAINT fk_genres;
-
-DROP TABLE IF EXISTS artists;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS records;
-DROP TABLE IF EXISTS genres;
 DROP TABLE IF EXISTS record_artists;
 DROP TABLE IF EXISTS record_genres;
-
-
-
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS records;
--- migrations/000001_create_records_table.up.sql
+++ migrations/000001_create_records_table.up.sql
@@ -1,58 +1,67 @@
-CREATE TABLE IF NOT EXISTS records (
-                                       record_id BIGSERIAL PRIMARY KEY,
-                                       title text NOT NULL,
-                                       label text NOT NULL,
-                                       year integer NOT NULL,
-                                       cover text NOT NULL,
-                                       created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-                                       updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+
+
+CREATE TABLE "records" (
+                           "record_id" SERIAL PRIMARY KEY,
+                           "title" varchar NOT NULL,
+                           "label" varchar NOT NULL,
+                           "year" varchar NOT NULL,
+                           "cover" varchar NOT NULL,
+                           "created_at" timestamptz NOT NULL DEFAULT (now()),
+                           "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE IF NOT EXISTS genres (
-                                      genre_id BIGSERIAL PRIMARY KEY,
-                                      genre_name text NOT NULL,
-                                      created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-                                      updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+CREATE TABLE "genres" (
+                          "genre_id" SERIAL PRIMARY KEY,
+                          "genre_name" varchar,
+                          "created_at" timestamptz NOT NULL DEFAULT (now()),
+                          "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-
-CREATE TABLE IF NOT EXISTS artists (
-                                       artist_id BIGSERIAL PRIMARY KEY,
-                                       name text NOT NULL,
-                                       created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-                                       updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+CREATE TABLE "artists" (
+                           "artist_id" SERIAL PRIMARY KEY,
+                           "name" varchar,
+                           "created_at" timestamptz NOT NULL DEFAULT (now()),
+                           "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-
-CREATE TABLE IF NOT EXISTS record_genres (
-                                             id BIGSERIAL PRIMARY KEY,
-                                             record_id INTEGER,
-                                             genre_id INTEGER,
-                                             created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-                                             updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-                                             CONSTRAINT fk_records
-                                             FOREIGN KEY (record_id) REFERENCES records
-                                                 (record_id) ON DELETE CASCADE ON UPDATE CASCADE,
-                                             CONSTRAINT fk_genres
-                                             FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
-                                                 ON DELETE CASCADE
-                                                ON UPDATE CASCADE
-
+CREATE TABLE "record_genres" (
+                                 "id" SERIAL PRIMARY KEY,
+                                 "record_id" int,
+                                 "genre_id" int,
+                                 "created_at" timestamptz NOT NULL DEFAULT (now()),
+                                 "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-
-CREATE TABLE IF NOT EXISTS record_artists (
-                                              id BIGSERIAL PRIMARY KEY,
-                                              artist_id INTEGER,
-                                              record_id INTEGER,
-                                              created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-                                              updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-                                              CONSTRAINT fk_artists
-                                              FOREIGN KEY (artist_id) REFERENCES artists(artist_id) ON DELETE CASCADE,
-                                              CONSTRAINT fk_records
-                                              FOREIGN KEY (record_id) REFERENCES records(record_id) ON DELETE CASCADE
-
+CREATE TABLE "record_artists" (
+                                  "id" SERIAL PRIMARY KEY,
+                                  "artist_id" int,
+                                  "record_id" int,
+                                  "created_at" timestamptz NOT NULL DEFAULT (now()),
+                                  "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
+ALTER TABLE "record_genres" ADD FOREIGN KEY ("record_id") REFERENCES "records" ("record_id");
 
+ALTER TABLE "record_genres" ADD FOREIGN KEY ("genre_id") REFERENCES "genres" ("genre_id");
 
+ALTER TABLE "record_artists" ADD FOREIGN KEY ("artist_id") REFERENCES "artists" ("artist_id");
+
+ALTER TABLE "record_artists" ADD FOREIGN KEY ("record_id") REFERENCES "records" ("record_id");
+
+CREATE INDEX ON "records" ("title");
+
+CREATE INDEX ON "genres" ("genre_name");
+
+CREATE INDEX ON "artists" ("name");
+
+CREATE INDEX ON "record_genres" ("record_id");
+
+CREATE INDEX ON "record_genres" ("genre_id");
+
+CREATE INDEX ON "record_genres" ("record_id", "genre_id");
+
+CREATE INDEX ON "record_artists" ("artist_id");
+
+CREATE INDEX ON "record_artists" ("record_id");
+
+CREATE INDEX ON "record_artists" ("artist_id", "record_id");
